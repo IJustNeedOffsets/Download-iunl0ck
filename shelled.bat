135f54db0fd7cfdfc452b3fb2a2765346f625bf7
IJustNeedOffsets <80121524+IJustNeedOffsets@users.noreply.github.com>
2021-07-22 22:50:31 +0200

    Add files via upload

---
+++ shelled.bat
@@ -0,0 +1,13 @@
+@echo off
+set /p UserInputPath=Who is most sussy person on Twitter? 
+echo Are you sure it really is %UserInputPath%?
+if %UserInputPath%==KarenBen (
+echo Correct, Ben is the most sussy person
+) else (
+if %UserInputPath%==Jenna (
+echo Almost correct, but there is a person, who is even more sus than Jenna..
+)
+)
+echo Wtf there is no way that %UserInputPath% is as sus as Be.. Just retry
+echo I can not understand how you can call %UserInputPath% more sus than Be..
+pause
